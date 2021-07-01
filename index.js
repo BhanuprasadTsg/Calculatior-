8d3e8b409bdc2dabe3ee14618f7fc2182e490c70
BhanuprasadTsg <bhanuthadaka9@gmail.com>
2021-07-01 20:06:14 +0530

    change

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.get("/",function(req,res){
     res.sendFile(__dirname + "/index.html");
 });
 
+
 app.post("/",function(req,res){
     var num1=Number(req.body.num1);
     var num2=Number(req.body.num2);
@@ -18,6 +19,15 @@ app.post("/",function(req,res){
     res.send("thanks your result=" + result);
 });
 
+app.get("/Bmicalculator",function(req,res){
+    res.sendFile(__dirname + "/Bmi.html")
+});
+app.post("/Bmicalculator",function(req,res){
+    var weight=parseFloat(req.body.weight);
+    var height=parseFloat(req.body.height);
+    var result=weight/(height+height);
+    res.send("your Bmi is" + result);
+});
 
 app.listen(8080,function(){
     console.log("server is running on port 8080");
